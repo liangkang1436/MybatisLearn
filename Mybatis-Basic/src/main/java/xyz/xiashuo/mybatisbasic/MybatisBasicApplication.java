ee857b136c0e549e5eeb4b7a07b2b57fe23b134e
liangkang <liangkang1436@163.com>
2022-10-25 09:31:58 +0800

    init commit

---
+++ Mybatis-Basic/src/main/java/xyz/xiashuo/mybatisbasic/MybatisBasicApplication.java
@@ -0,0 +1,13 @@
+package xyz.xiashuo.mybatisbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisBasicApplication.class, args);
+    }
+
+}
---
+++ Mybatis-Basic/src/test/java/xyz/xiashuo/mybatisbasic/MybatisBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xiashuo.mybatisbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
